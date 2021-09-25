54c2dc37888b3d9c36c15c40f654248c7e88354c
Ibrahim Khan <aitvetibrahim@gmail.com>
2021-09-25 23:52:07 +0600

    cart added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <img className="logo" src={logo} alt="" />
             <nav>
                 <a href="/shop">Shop</a>
-                <a href="/orders">Orders</a>
+                <a href="/orders">Order Review</a>
                 <a href="/inventory">Manage Inventory</a>
             </nav>
         </div>
