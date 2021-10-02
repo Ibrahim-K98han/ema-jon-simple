b88cb6d699b5715b4640f29816acba7fca985567
Ibrahim Khan <aitvetibrahim@gmail.com>
2021-10-03 02:05:48 +0600

    added router and hook

---
+++ src/components/OrderReview/OrderReview.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import useProducts from '../../Hooks/useProducts';
+import userCart from '../../Hooks/useCart';
+import useCart from '../../Hooks/useCart';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
+
+const OrderReview = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart);
+        removeFromDb(key);
+
+    }
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        product={product}
+                        handleRemove={handleRemove}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default OrderReview;
